ecc33057a61438df1e9a48330834b7898b5b76d0
d-apurva <apurva.dhoble@click2cloud.net>
2018-02-26 17:22:26 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 echo "Your software installation script"
 docker run -d -it -p 9000:9000  -v /home/userdata:/click2cloud-dockm/data  -v /var/run/docker.sock:/var/run/docker.sock:z --name click2cloud-dockm-s2i click2cloud/helloworldapp
 
-
+#
+# docker is available at docker -H tcp://0.0.0.0:2375
+#
